59bdde49d60f800b1b7fbcce2178ddf05d0f3472
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-02-25 11:52:02 +0500

    modified work

--- TypeScript-Practice/main.js
+++ TypeScript-Practice/main.js
@@ -1,3 +1,7 @@
 // let  personName = "Haseeb Ur Rehman"
 // console.log("Hello", personName, "would like to learn some python coding")
-console.log("Hello World");
+// console.log("Hello World");
+var userName = "Haseeb Ur Rehman";
+console.log(userName);
+userName = "is governor of sindh";
+console.log(userName);
--- TypeScript-Practice/main.ts
+++ TypeScript-Practice/main.ts
@@ -1,6 +1,13 @@
 // let  personName = "Haseeb Ur Rehman"
 // console.log("Hello", personName, "would like to learn some python coding")
 
-console.log("Hello World");
+
+// console.log("Hello World");
+
+let userName = "Haseeb Ur Rehman";
+console.log(userName);
+userName = "is governor of sindh"
+console.log(userName)
 
 
+let lastName: string = "Haseeb Ur Rehman"
