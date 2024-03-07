abea6664fc04393389ace9cf5997f491c9eeed4f
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-03-07 12:10:54 +0500

    modifeid worj

---
+++ #11-Names/main.js
@@ -0,0 +1,7 @@
+// let friendNames = ['Haseeb', 'Jawad', 'Ahmed', 'Abdullah']
+// console.log(friendNames);
+// friendNames.forEach(friendsName => console.log(friendsName)
+// );
+var friendsName = ["Haseeb", "Jawad", "Ahmed", "Farooq"];
+console.log(friendsName);
+friendsName.forEach(function (friendName) { return console.log(friendName); });
---
+++ #11-Names/main.ts
@@ -0,0 +1,12 @@
+// let friendNames = ['Haseeb', 'Jawad', 'Ahmed', 'Abdullah']
+// console.log(friendNames);
+
+// friendNames.forEach(friendsName => console.log(friendsName)
+// );
+
+let friendsName = ["Haseeb", "Jawad", "Ahmed", "Farooq"]
+console.log(friendsName);
+
+friendsName.forEach(friendName => console.log(friendName);
+)
+
