24f65a499c7efc3b92099e06250c35cfe3ddc369
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-03-23 07:42:19 +0500

    modified work

---
+++ 15. Changing Guest List/main.ts
@@ -0,0 +1,19 @@
+let guestList :string[] = ['Imran Khan','Nawaz Sharif','Asif Ali Zardari'];
+
+for (let i = 0; i < guestList.length; i++) {
+    console.log(`Dear Mr, ${guestList[i]}It is our pleasure to invite you in our party.\n\nThank You!\n\n`);   
+}
+
+let absentGuest = 'Imran khan'
+let newguest = 'Kamran Tessori'
+
+guestList[0] = newguest
+
+for (let i = 0; i < guestList.length; i++){
+    console.log('Dear Mr. ' + guestList[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!\n\n')
+
+}
+
+console.log(`Mr. ${absentGuest} is not coming to the party.`)
+
+
