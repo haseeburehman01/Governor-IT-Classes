e9944fc556598b0ee09d8771194b7d6c3101e0e0
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-03-23 07:24:36 +0500

    modifeid work

---
+++ 14-Guest List/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let gustList = ['Nawaz Sharif', 'Asif Ali Zardari', 'Imran Khan'];
+for (let i = 0; i < gustList.length; i++) {
+    console.log(`Dear Mr ${gustList[i]}, It is our pleasure to invite you in our party.\n\nThank You!'`);
+}
---
+++ 14-Guest List/main.ts
@@ -0,0 +1,6 @@
+let gustList :string[] = ['Nawaz Sharif', 'Asif Ali Zardari', 'Imran Khan'];
+
+for (let i = 0; i < gustList.length; i++){
+    console.log(`Dear Mr ${gustList[i]}, It is our pleasure to invite you in our party.\n\nThank You!'`);
+    
+}
