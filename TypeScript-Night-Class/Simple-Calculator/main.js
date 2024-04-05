aeeadffd111eb7a8729fdfe258e79902e0f3d00f
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-04-05 16:40:09 +0500

    modfoed work

---
+++ TypeScript-Night-Class/Simple-Calculator/main.js
@@ -0,0 +1,32 @@
+import inquirer from 'inquirer';
+const answer = await inquirer.prompt([
+    {
+        name: 'firstnumber',
+        type: 'number',
+        message: 'Enter Your First Number'
+    },
+    {
+        name: 'secondnumber',
+        type: 'number',
+        message: 'Enter Your Second Number'
+    },
+    {
+        message: 'Select Of the one operator to perfrom Action.',
+        type: 'list',
+        name: 'operator',
+        choices: ['+', '-', '*', '/']
+    }
+]);
+console.log(answer);
+if (answer.operator === '+') {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === '-') {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === '*') {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === '/') {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
