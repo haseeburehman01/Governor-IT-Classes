db02bfc03fec98f68b216dd4df790361e634af9a
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-04-28 10:04:27 +0500

    modified work

---
+++ Practice-TypeScript/main.js
@@ -0,0 +1,3 @@
+var isLogin = true;
+var checked = isLogin ? 'You are login' : 'First you login';
+console.log(checked);
---
+++ Practice-TypeScript/main.ts
@@ -0,0 +1,15 @@
+// let isLogin = false;
+// let checked :string = isLogin ? 'You are login' : 'First you login'
+
+// console.log(checked);
+
+let email = 'example@gmail.com';
+let password = 'abcd1234'
+
+let islogined = false
+
+if (email && password){
+    islogined = true;
+    console.log('login ');
+    
+}
