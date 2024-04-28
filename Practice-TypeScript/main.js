55a23d804bd4ce3b0525a0a827de4e3f4c14432d
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-04-28 11:36:03 +0500

    modified work

--- Practice-TypeScript/main.js
+++ Practice-TypeScript/main.js
@@ -1,3 +1,26 @@
-var isLogin = true;
-var checked = isLogin ? 'You are login' : 'First you login';
-console.log(checked);
+"use strict";
+// let isLogin = false;
+// let checked :string = isLogin ? 'You are login' : 'First you login'
+// console.log(checked);
+// let email = 'example@gmail.com';
+// let password = 'abcd1234'
+// let islogined = false
+// if (email && password){
+//     islogined = true;
+//     console.log('login ');
+// }
+let dayName = 'monday';
+switch (dayName) {
+    case 'sunday':
+        console.log('You are in class ');
+        break;
+    case 'monday':
+        console.log('khan ha bhai');
+        break;
+    case 'tuesday':
+        console.log('ap ay nahi ho');
+}
+let halfHungry = function () {
+    console.log('hers is haseeb');
+};
+halfHungry();
--- Practice-TypeScript/main.ts
+++ Practice-TypeScript/main.ts
@@ -3,13 +3,33 @@
 
 // console.log(checked);
 
-let email = 'example@gmail.com';
-let password = 'abcd1234'
+// let email = 'example@gmail.com';
+// let password = 'abcd1234'
 
-let islogined = false
+// let islogined = false
 
-if (email && password){
-    islogined = true;
-    console.log('login ');
-    
-}
+// if (email && password){
+//     islogined = true;
+//     console.log('login ');
+// }
+// let dayName = 'monday'
+
+// switch (dayName) {
+//     case 'sunday':
+//         console.log('You are in class ');
+//         break
+//     case 'monday':
+//         console.log('khan ha bhai');
+//         break
+//         case 'tuesday':
+//             console.log('ap ay nahi ho');
+// }
+
+// let halfHungry = function(){
+//     console.log('hers is haseeb');
+// }
+
+// halfHungry()
+
+
+function 
