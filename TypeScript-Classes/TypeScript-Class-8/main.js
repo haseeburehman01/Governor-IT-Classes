b58acb40ab79bc2123125834e6b09c16382892b1
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-05-05 10:52:26 +0500

    modified work

---
+++ TypeScript-Classes/TypeScript-Class-8/main.js
@@ -0,0 +1,13 @@
+"use strict";
+// Start Slides 204 From Hosting.
+// TDZ stand For temporal Red Zone,
+// ===> Recursive Function 
+// function recursiveFunction(condition: number) {
+//     if (condition >= 0) {
+//         console.log('Haseeb Ur Rehman');
+//     }
+//     recursiveFunction(condition - 1)
+// } 
+// recursiveFunction(5)
+console.log(x);
+var x = 10;
