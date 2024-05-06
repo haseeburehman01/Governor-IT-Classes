8b23a280838885299c3b9bef0840bb8fa0311b28
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-05-06 13:21:20 +0500

    modified work

---
+++ TypeScript-45-Assignment/34. Pizzas/Pizzas.js
@@ -0,0 +1,9 @@
+"use strict";
+//  names of the pizzas
+let pizzas = ["Pepperoni", "Cuban", "BBQ Chicken"];
+// Loop Use kiya he.
+for (let pizza of pizzas) {
+    console.log(`I like ${pizza} pizza.`);
+}
+// Result
+console.log("I really love pizza!");
---
+++ TypeScript-45-Assignment/38. Cities/Cities.ts
@@ -0,0 +1,9 @@
+// Set describe_city Fucntion
+function describe_city(cityName: string, country: string = 'DefaultCountry') {
+  console.log(`${cityName} is in ${country}.`);
+}
+
+// Result
+describe_city('Karachi', 'Pakistan');
+describe_city('Tokyo', 'Japan');
+describe_city('Paris');
