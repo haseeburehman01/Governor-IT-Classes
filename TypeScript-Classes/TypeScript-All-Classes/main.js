4894ecad353c4042a83fb8e785a1886bde705f06
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-05-07 09:42:46 +0500

    modified work

---
+++ TypeScript-Classes/TypeScript-All-Classes/main.js
@@ -0,0 +1,14 @@
+// Strong typing  in TypeScript!
+var userName = 'Haseeb Ur Rehman'; // ===> type inference Auto detected type. 
+var user = 'Haseeb Ur Rehman'; // ===> Strong Typing in TypeScript.
+// Premitive Data Types!
+// Strongly Typed Syntax!
+var fName = 'Haseeb';
+var Age = 17;
+var listStudent = true;
+var randomNumber = 'Haseeb ur rehman';
+// let & Const? Diffrence 
+var mony = 50;
+mony = 20; // ===> ReAsgin Value 
+var BankNumber = 12345657;
+BankNumber = 23123123; // ERROR Becouse it is constant Varibale!
---
+++ TypeScript-Classes/TypeScript-All-Classes/main.ts
@@ -0,0 +1,19 @@
+// Strong typing  in TypeScript!
+let userName = 'Haseeb Ur Rehman' // ===> type inference Auto detected type. 
+let user = 'Haseeb Ur Rehman' // ===> Strong Typing in TypeScript.
+
+// Premitive Data Types!
+// Strongly Typed Syntax!
+
+let fName :string = 'Haseeb';
+let Age :number = 17;
+let listStudent :boolean = true
+let randomNumber :any = 'Haseeb ur rehman'
+
+// let & Const? Diffrence 
+
+let mony = 50;
+mony = 20 // ===> ReAsgin Value 
+
+const BankNumber = 12345657;
+BankNumber = 23123123; // ERROR Becouse it is constant Varibale!
