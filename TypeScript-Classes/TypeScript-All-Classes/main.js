373a3a9cebeee2f3de1c275e7d54a75008187a42
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-05-07 10:11:14 +0500

    modified work

--- TypeScript-Classes/TypeScript-All-Classes/main.js
+++ TypeScript-Classes/TypeScript-All-Classes/main.js
@@ -1,14 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // Strong typing  in TypeScript!
-var userName = 'Haseeb Ur Rehman'; // ===> type inference Auto detected type. 
-var user = 'Haseeb Ur Rehman'; // ===> Strong Typing in TypeScript.
+let userName = 'Haseeb Ur Rehman'; // ===> type inference Auto detected type. 
+let user = 'Haseeb Ur Rehman'; // ===> Strong Typing in TypeScript.
 // Premitive Data Types!
 // Strongly Typed Syntax!
-var fName = 'Haseeb';
-var Age = 17;
-var listStudent = true;
-var randomNumber = 'Haseeb ur rehman';
+let FName = 'Haseeb';
+let Age = 17;
+let listStudent = true;
+let randomNumber = 'Haseeb ur rehman';
 // let & Const? Diffrence 
-var mony = 50;
+let mony = 50;
 mony = 20; // ===> ReAsgin Value 
-var BankNumber = 12345657;
-BankNumber = 23123123; // ERROR Becouse it is constant Varibale!
+const BankNumber = 12345657;
+// BankNumber = 23123123; // ERROR Becouse it is constant Varibale!
+//String Concatination 
+let firstName = 'Haseeb';
+let lastName = 'Ur Rehman';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+// Template Literals
+let fName = 'Haseeb';
+let lName = 'Rajput';
+let FullName = `${fName} ${lName}`;
+console.log(FullName);
+// Operators in TypeScript
+// Addition +
+// Subtraction -
+// Multiplications *
+// Division / 
+// Exponentiation ** // Give me Power
+// Modules % Give me Remainder 
+// Unary Oprators
+// ++98 is this prefix
+// 98++ is this postfix
+// Assginment Oprators 
+// 5 == "6" Is False 
+// 5 != 2 Is True becouse its give me Answer oposite Direaction like true hoto false. False hoto True.
--- TypeScript-Classes/TypeScript-All-Classes/main.ts
+++ TypeScript-Classes/TypeScript-All-Classes/main.ts
@@ -5,7 +5,7 @@ let user = 'Haseeb Ur Rehman' // ===> Strong Typing in TypeScript.
 // Premitive Data Types!
 // Strongly Typed Syntax!
 
-let fName :string = 'Haseeb';
+let FName :string = 'Haseeb';
 let Age :number = 17;
 let listStudent :boolean = true
 let randomNumber :any = 'Haseeb ur rehman'
@@ -16,4 +16,35 @@ let mony = 50;
 mony = 20 // ===> ReAsgin Value 
 
 const BankNumber = 12345657;
-BankNumber = 23123123; // ERROR Becouse it is constant Varibale!
+// BankNumber = 23123123; // ERROR Becouse it is constant Varibale!
+
+//String Concatination 
+let firstName :string = 'Haseeb'
+let lastName :string = 'Ur Rehman'
+let fullName :string = firstName + ' ' + lastName;
+console.log(fullName);
+
+// Template Literals
+let fName :string = 'Haseeb'
+let lName :string = 'Rajput'
+let FullName :string = `${fName} ${lName}`
+console.log(FullName);
+
+// Operators in TypeScript
+
+// Addition +
+// Subtraction -
+// Multiplications *
+// Division / 
+// Exponentiation ** // Give me Power
+// Modules % Give me Remainder 
+
+// Unary Oprators
+
+// ++98 is this prefix
+// 98++ is this postfix
+
+// Assginment Oprators 
+
+// 5 == "6" Is False 
+// 5 != 2 Is True becouse its give me Answer oposite Direaction like true hoto false. False hoto True.
