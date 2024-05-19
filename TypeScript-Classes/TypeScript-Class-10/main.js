14f03abddce01812c56a170fcced6be4dcc6ec31
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-05-19 11:50:54 +0500

    modified work

---
+++ TypeScript-Classes/TypeScript-Class-10/main.js
@@ -0,0 +1,17 @@
+"use strict";
+// fresh object and stail object a = b dono alag object
+// revise employ project
+// arror function kya hai or isy array mein use karty hain
+// cover all loops 4 loops
+// paragrimiz website for loops details
+// length for loop se define kare hain 
+//  lenght hmesha 1 se start hoti hai. or index zero se start hoti hai
+let CollectionOfEmployee = {
+    employeeId: 'jhasdkjahg',
+    employeeName: 'haseeb',
+    employeeSalray: 298310
+};
+let student = ['hjash', 'jkhsd', 'ujhajksd', 'jhsa'];
+for (let i = 0; i <= student.length; i++) {
+    console.log(student[i]);
+}
---
+++ TypeScript-Classes/TypeScript-Class-10/main.ts
@@ -0,0 +1,29 @@
+// fresh object and stail object a = b dono alag object
+// revise employ project
+// arror function kya hai or isy array mein use karty hain
+// cover all loops 4 loops
+// paragrimiz website for loops details
+// length for loop se define kare hain 
+//  lenght hmesha 1 se start hoti hai. or index zero se start hoti hai
+
+// KEY VALUE HOLD KARA GA ==> CROSSPONDING VALUE  
+// ARROW FUNCTION ==> (SALARY) => SALARY > 50000;
+
+interface employee{
+    employeeId : string
+    employeeName : string
+    employeeSalray : number
+}
+
+let CollectionOfEmployee:employee = {
+    employeeId : 'jhasdkjahg',
+    employeeName : 'haseeb',
+    employeeSalray : 298310
+}
+
+let student = ['hjash','jkhsd','ujhajksd','jhsa']
+
+for(let i = 0; i <= student.length; i++){
+    console.log(student[i]);
+}
+ 
