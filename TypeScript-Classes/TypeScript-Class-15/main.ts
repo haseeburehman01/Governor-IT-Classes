7c176de71f6df02e3e63c1642c8e7b6bec8df664
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-07-07 09:57:19 +0500

    typescript class 15

---
+++ TypeScript-Classes/TypeScript-Class-15/main.ts
@@ -0,0 +1,7 @@
+// Tuple ===> tuples ak array ha jisma hum array ki value define krta hain 
+// Enums ===> by default enums jo ha phali value ko 0 sa define krta ha 
+//  Union Types ===> wo type ha jo ak variable ko two type assgin krta ha 
+
+// Type Aliases and Interfaces
+// Basic Generics
+// REST PARAMETOR
